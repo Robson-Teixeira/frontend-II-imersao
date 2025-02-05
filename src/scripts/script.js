03d48d22d2b8c237842f24d8a4da75e6f84f5617
Robson-Teixeira <robsonmteixeira@outlook.com>
2025-02-05 01:25:04 -0300

    Aula 04: Desafio

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,19 +8,32 @@ function requestApi(searchTerm) {
 
     fetch(url)
         .then((response) => response.json())
-        .then((result) => displayResults(result));
+        .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(result) {
+function displayResults(result, searchTerm) {
 
     resultPlaylist.classList.add("hidden");
 
-    const artistName = document.getElementById('artist-name');
-    const artistImage = document.getElementById('artist-img');
+    const artistCard = document.querySelector('.artist-card');
 
-    result.forEach(element => {
-        artistName.innerText = element.name;
-        artistImage.src = element.urlImg;
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = ''; // Limpa os resultados anteriores
+
+    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm));
+
+    filteredArtists.forEach(artist => {
+
+        const clonedArtistCard = artistCard.cloneNode(true);
+
+        // div.card-img > img.artist-img
+        clonedArtistCard.children[0].children[0].src = artist.urlImg;
+        // div.card-text > a.vst
+        clonedArtistCard.children[1].children[0].title = artist.name;
+        // div.card-text > span.artist-name
+        clonedArtistCard.children[1].children[1].innerHTML = artist.name;
+
+        gridContainer.appendChild(clonedArtistCard);
     });
 
     resultArtist.classList.remove('hidden');
@@ -28,7 +41,7 @@ function displayResults(result) {
 
 document.addEventListener('input', function () {
 
-    const searchTerm = searchInput.value.toLowerCase();
+    const searchTerm = searchInput.value.toLowerCase().trim();
 
     if (searchTerm === '') {
         resultPlaylist.classList.remove('hidden');
